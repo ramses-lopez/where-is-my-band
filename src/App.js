3707801c0b972b1d4ce0d99cca6b80c46c1e4ff2
Irving Ventura <irvingv8@gmail.com>
2017-11-23 16:29:28 -0400

    -> Developed Browser Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { ThemeProvider } from "styled-components";
+import Browser from "./Components/Browser";
 
 const theme = {
   primary: "#7B1FA2"
@@ -9,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <div>WIMB</div>
+        <Browser />
       </ThemeProvider>
     );
   }
---
+++ src/Components/Browser/index.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Form = styled.form`
+  position: relative;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+
+  &:after {
+    content: "";
+    box-shadow: 0px 0px 25px #000;
+    display: block;
+    width: 300px;
+    height: 5px;
+    position: absolute;
+    z-index: -1;
+    bottom: 0;
+  }
+`;
+
+const Input = styled.input.attrs({
+  type: "text"
+})`
+  background: white;
+  border: none;
+  font-size: 16px;
+  line-height: 50px;
+  height: 50px;
+  width: 100%;
+  max-width: 300px;
+  border-radius: 3px 0px 0px 3px;
+  outline: none;
+  padding-left: 15px;
+`;
+
+const Icon = styled.button.attrs({
+  type: "submit"
+})`
+  border: none;
+  background: #7b1fa2;
+  color: white;
+  height: 50px;
+  line-height: 50px;
+  font-size: 20px;
+  padding: 0px 15px;
+  border-radius: 0px 3px 3px 0px;
+`;
+
+class Browser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { search: "" };
+
+    // Binding functions
+    this.handleForm = this.handleForm.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  // controlling the input field
+  handleChange(e) {
+    this.setState({ search: e.target.value });
+  }
+
+  // handling the form submission this will call a
+  // function passed as a prop from it's parent component named "search"
+  handleForm(e) {
+    e.preventDefault();
+    this.props.search(this.state.search);
+  }
+
+  render() {
+    return (
+      <Form onSubmit={this.handleForm}>
+        <Input
+          type="text"
+          placeholder="Search"
+          value={this.state.search}
+          onChange={this.handleChange}
+        />
+        <Icon className="fa fa-search" aria-hidden="true" />
+      </Form>
+    );
+  }
+}
+
+export default Browser;
