9fbdf4b6b0ca94ea80d7e85fb997b9a6ca85d341
Irving Ventura <irvingv8@gmail.com>
2017-11-25 02:23:16 -0400

    -> changed pic from img to background and using background-size cover

--- src/Components/Artist/Picture.js
+++ src/Components/Artist/Picture.js
@@ -1,8 +1,12 @@
 import styled from "styled-components";
 
-const Picture = styled.img`
+const Picture = styled.div`
   width: 100%;
-  height: auto;
+  height: 300px;
+  background-image: url(${props => props.src});
+  background-size: cover;
+  background-position: center;
+  border: 0px;
   margin: auto;
   display: block;
 `;
